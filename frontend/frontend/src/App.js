03f03bc90cc1e8914d313427f5891a0f13b511f9
Kai Ma <jackmk.kai@gmail.com>
2019-05-11 17:03:48 +0200

    state values not working

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -5,32 +5,32 @@ import './App.css';
 import SearchFiled from './search';
 import EvenetField from './event';
 import 'bootstrap/dist/css/bootstrap.css';
-import {ListGroup, Button, Container,Col, Row, Popover} from 'react-bootstrap';
+import {ListGroup, Button, Container, Col, Row, Popover} from 'react-bootstrap';
 import {geolocated} from 'react-geolocated';
 
 
-class App extends React.Component{
-    constructor(props, context){
-    super(props,context);
+class App extends React.Component {
+    constructor(props, context) {
+        super(props, context);
 
-    this.state = {
-        haveSearched: false,
-        result: null,
-        latest: null
-      }
-    this.changeState = this.changeState.bind(this);
-    this.greetParent = this.greetParent.bind(this);
-    this.changeModal = this.changeModal.bind(this);
-    this.sendQuery = this.sendQuery.bind(this);
-    this.findEvent = this.findEvent.bind(this);
-    this.sendHistory = this.sendHistory.bind(this);
+        this.state = {
+            haveSearched: false,
+            result: null,
+            latest: null
+        }
+        this.changeState = this.changeState.bind(this);
+        this.greetParent = this.greetParent.bind(this);
+        this.changeModal = this.changeModal.bind(this);
+        this.sendQuery = this.sendQuery.bind(this);
+        this.findEvent = this.findEvent.bind(this);
+        this.sendHistory = this.sendHistory.bind(this);
 
 
-  }
+    }
 
     changeState(e) {
-        if(this.state.latest != null){
-            console.log("Latest = "+this.state.latest + " id= " + e)
+        if (this.state.latest != null) {
+            console.log("Latest = " + this.state.latest + " id= " + e)
             //HÄR SKICKAAR VI updatend till om från till
 
         }
@@ -42,32 +42,62 @@ class App extends React.Component{
         alert(`Hella ${this.state.latest}`)
     }
 
-    sendQuery (query){
-        
-        let term = 'http://localhost:8080/documents/search/' + query.query
-        fetch(term,{
-          method: "GET",
-          mode: "cors",
-          headers: {
-            "Content-Type": "application/json",
-          }
-        })
-        .then((response) => response.json())
-        .then((res) => {
-          alert("i fetch")
-          //HÄR SPARAR VI NER INFON FRÅN BACKEND"
-            console.log(res);
-
-          this.setState({result: res})
-          if(!this.state.haveSearched){
-            this.setState({haveSearched: true})
+    sendQuery(query) {
+        let term = 'http://localhost:8080/documents/search/'
+        const input = {
+            'query': query.query,
+            'music': this.state.music,
+            'businessAndProfessional': this.state.businessAndProfessional,
+            'foodAndDrink': this.state.foodAndDrink,
+            'communityAndCulture': this.state.communityAndCulture,
+            'performingAndVisualArts': this.state.performingAndVisualArts,
+            'filmAndMediaAndEntertainment': this.state.filmAndMediaAndEntertainment,
+            'sportsAndFitness': this.state.sportsAndFitness,
+            'healthAndWellness': this.state.healthAndWellness,
+            'scienceAndTechnology': this.state.scienceAndTechnology,
+            'travelAndOutdoor': this.state.travelAndOutdoor,
+            'charityAndCauses': this.state.charityAndCauses,
+            'religionAndSpirituality': this.state.religionAndSpirituality,
+            'familyAndEducation': this.state.familyAndEducation,
+            'seasonalAndHoliday': this.state.seasonalAndHoliday,
+            'governmentAndPolitics': this.state.governmentAndPolitics,
+            'fashionAndBeauty': this.state.fashionAndBeauty,
+            'homeAndLifestyle': this.state.homeAndLifestyle,
+            'autoAndBoatAndAir': this.state.autoAndBoatAndAir,
+            'hobbiesAndSpecialAndInterest': this.state.hobbiesAndSpecialAndInterest,
+            'other': this.state.other,
+            'schoolActivities': this.state.schoolActivities,
+            'longitude': this.state.long,
+            'latitude': this.state.lat,
+            'fromDate': this.state.fromDate,
+            'toDate': this.state.toDate,
+            'isLocataionChecked': this.state.isLocataionChecked
         }
-        }).catch((err) => alert(err))
+        console.log(input)
+        fetch(term, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(input)
+        })
+            .then((response) => response.json())
+            .then((res) => {
+                alert("i fetch")
+                //HÄR SPARAR VI NER INFON FRÅN BACKEND"
+                console.log(res);
+
+                this.setState({result: res})
+                if (!this.state.haveSearched) {
+                    this.setState({haveSearched: true})
+                }
+            }).catch((err) => alert(err))
 
 
     }
 
-    sendHistory (id){
+    sendHistory(id) {
         alert("SEND")
 
     }
@@ -76,66 +106,66 @@ class App extends React.Component{
         console.log(e)
         console.log(this.state.result.length);
         for (var i = 0; i < this.state.result.length; i++) {
-          console.log(this.state.result[i]._id)
-          if(e === this.state.result[i]._id){
-            return true
-          }
+            console.log(this.state.result[i]._id)
+            if (e === this.state.result[i]._id) {
+                return true
+            }
         }
-      }
-
-  locateEvent () {
-    this.event2.handleShow();
-  }
-
-  changeModal (e) {
-    if(this.findEvent(e)){
-      this[`event${e}`].handleShow();
     }
-    
-  }
 
-  render() {
-      if(this.state.haveSearched){
-        return(
-          <div>
-            <div className="header">
-              <Col>
-                <img src={logo} className="logo" alt="logo" />
-              </Col>
-              <Col >
-                <div className = "searchf">
-                  <SearchFiled sendQuery = {this.sendQuery} />
-                </div>
-              </Col>
-              <Col >
-              </Col>
-            </div>
-            <div>
-                    <ListGroup as="ul">
-          {this.state.result.map((d) => {
-                    return (<EvenetField key = {d._id} id= {d._id} ref = {(e) => {
-                      const nr = d.id;
-                      this[`event${d._id}`] = e;
-                    }} name = {d.eventName} summary = {d.summary}text = {d.text} greetHandler = {this.changeState} changeEvent = {this.changeModal}/>)
-                  })}
-        </ListGroup>
-          </div>
-          </div>
+    locateEvent() {
+        this.event2.handleShow();
+    }
 
-          )
-      }else{
+    changeModal(e) {
+        if (this.findEvent(e)) {
+            this[`event${e}`].handleShow();
+        }
+
+    }
+
+    render() {
+        if (this.state.haveSearched) {
             return (
+                <div>
+                    <div className="header">
+                        <Col>
+                            <img src={logo} className="logo" alt="logo"/>
+                        </Col>
+                        <Col>
+                            <div className="searchf">
+                                <SearchFiled sendQuery={this.sendQuery}/>
+                            </div>
+                        </Col>
+                        <Col>
+                        </Col>
+                    </div>
+                    <div>
+                        <ListGroup as="ul">
+                            {this.state.result.map((d) => {
+                                return (<EvenetField key={d._id} id={d._id} ref={(e) => {
+                                    const nr = d.id;
+                                    this[`event${d._id}`] = e;
+                                }} name={d.eventName} summary={d.summary} text={d.text} greetHandler={this.changeState}
+                                                     changeEvent={this.changeModal}/>)
+                            })}
+                        </ListGroup>
+                    </div>
+                </div>
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <SearchFiled sendQuery = {this.sendQuery}/>        
-      </header>
-    </div>
-  );
-      }
+            )
+        } else {
+            return (
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <SearchFiled sendQuery={this.sendQuery}/>
+                    </header>
+                </div>
+            );
+        }
 
-  }
+    }
 }
 
 
--- frontend/frontend/src/search.js
+++ frontend/frontend/src/search.js
@@ -1,168 +1,221 @@
 import React from 'react';
 //import 'bootstrap/dist/css/bootstrap.css';
 import './search.css';
-import {FormControl, Form, Popover,OverlayTrigger, Button, Row, Col} from 'react-bootstrap';
+import {FormControl, Form, Popover, OverlayTrigger, Button, Row, Col} from 'react-bootstrap';
 import Geo from './geo';
 import {geolocated} from 'react-geolocated';
 import Calendar from 'react-calendar'
 
 
 class SearchField extends React.Component {
-	constructor(props){
-		super(props)
-		this.state = {
-			query: "",
-			"103": true,
-			"101": true,
-			"113": true,
-			"105": true,
-			"104": true,
-			"108": true,
-			"107": true,
-			"102": true,
-			"109": true,
-			"111": true,
-			"114": true,
-			"115": true,
-			"116": true,
-			"112": true,
-			"106": true,
-			"118": true,
-			"119": true,
-			"199": true,
-			"120": true,
-			long: null,
-			lat: null,
-			fromDate: new Date(),
-			toDate: new Date(),
-		}	
+    constructor(props) {
+        super(props)
+        this.state = {
+            query: "",
+            music: "",
+            businessAndProfessional: "",
+            foodAndDrink: "",
+            communityAndCulture: "",
+            performingAndVisualArts: "",
+            filmAndMediaAndEntertainment: "",
+            sportsAndFitness: "",
+            healthAndWellness: "",
+            scienceAndTechnology: "",
+            travelAndOutdoor: "",
+            charityAndCauses: "",
+            religionAndSpirituality: "",
+            familyAndEducation: "",
+            seasonalAndHoliday: "",
+            governmentAndPolitics: "",
+            fashionAndBeauty: "",
+            homeAndLifestyle: "",
+            autoAndBoatAndAir: "",
+            hobbiesAndSpecialAndInterest: "",
+            other: "",
+            schoolActivities: "",
+            long: null,
+            lat: null,
+            fromDate: new Date(),
+            toDate: new Date(),
+            isLocataionChecked: false
+        }
 
-	}
+    }
 
-	handleKeyPress(target) {
-		if(target.charCode==13){
-			alert(target.target.value);
-			//alert(this.state.query); 
-		}
-	}
-	onClickPreventDefault(e) {
-	    //alert('onClickPreventDefault called, form will not submit');
-	    e.preventDefault();
-	    //alert(e.target.value)
-		if (this.props.coords.longitude != null && this.props.coords.latitude != null) {
-			this.setState({long: this.props.coords.longitude, lat: this.props.coords.latitude});
-			console.log("Here is location " + this.props.coords.longitude + " " + this.props.coords.latitude);
-		}
-	    //alert(this.state.long)
-	    this.props.sendQuery(this.state);
-  	}
+    handleKeyPress(target) {
+        if (target.charCode == 13) {
+            alert(target.target.value);
+            //alert(this.state.query);
+        }
+    }
 
-  	updateCalendar = date => this.setState({ fromDate: date })
-  	updateCalendar2 = date => this.setState({ toDate: date })
-  	
-  	componentDidMount() {
-  		this.input.focus()
-  	}
+    onClickPreventDefault(e) {
+        //alert('onClickPreventDefault called, form will not submit');
+        e.preventDefault();
+        //alert(e.target.value)
+        if (this.props.coords.longitude != null && this.props.coords.latitude != null) {
+            this.setState({long: this.props.coords.longitude, lat: this.props.coords.latitude});
+        }
 
+        this.props.sendQuery(this.state);
+    }
 
-	render(){
-		return(
+    updateCalendar = date => this.setState({fromDate: date})
+    updateCalendar2 = date => this.setState({toDate: date})
 
-			<Form className ="searchField" onSubmit = {(e) =>{
-					this.onClickPreventDefault(e);
-				}}>
-				<Form.Group >
-					<Form.Control ref = {(e) => {this.input = e	}}type="text" placeholder="What event are you looking for?"  onChange= {(e)=> {this.setState({query: e.target.value})}}/>
-				</Form.Group>
-
-				<Row className="justify-content-md-center">
-				<Col>
-				  	<OverlayTrigger trigger="click" placement="auto" rootClose = "true" overlay={	
-				  		<Popover >
-				  			<Calendar onClickDay={this.updateCalendar} value={this.state.fromDate}/>
-						</Popover>}>
-    					<Button variant="link" className ="btn-cat">From date : {this.state.fromDate.toLocaleDateString()}</Button>
-  					</OverlayTrigger>
-  				</Col>
-  				<Col>
-				  <OverlayTrigger trigger="click" placement="auto"  rootClose = "true" overlay={				<Popover >
-				  	<Calendar onClickDay={this.updateCalendar2} value={this.state.toDate}/>
-				</Popover>}>
-    				<Button variant="link" className ="btn-cat">To date : {this.state.toDate.toLocaleDateString()}</Button>
-  					</OverlayTrigger>	
-  				</Col>
-  				<Col>			
-				  <OverlayTrigger trigger="click" placement="under" rootClose = "true" overlay={				<Popover >
-					<Form.Group controlId="formBasicChecbox">
-	    				<Form.Check id= {1} defaultChecked = "true" type="checkbox" inline label="Music" onChange = {(e) => {this.setState({"103": e.target.checked})}}/>
-	    				<Form.Check id= {2} defaultChecked = "true" type="checkbox" inline label="Business & Professional" onChange = {(e) => {this.setState({"101": e.target.checked})}}/>
-	    				<Form.Check id= {3} defaultChecked = "true" type="checkbox" inline label="Food & Drink" onChange = {(e) => {this.setState({"110": e.target.checked})}}/> 
-	    				<Form.Check id= {4} defaultChecked = "true" type="checkbox" inline label="Community & Culture" onChange = {(e) => {this.setState({"113": e.target.checked})}}/>
-	    				<Form.Check id= {5} defaultChecked = "true" type="checkbox" inline label="Performing & Visual Arts" onChange = {(e) => {this.setState({"105": e.target.checked})}} />
-	    				<Form.Check id= {6} defaultChecked = "true" type="checkbox" inline label="Film, Media & Entertainment" onChange = {(e) => {this.setState({"104": e.target.checked})}}/> 
-	    				<Form.Check id= {7} defaultChecked = "true" type="checkbox" inline label="Sports & Fitness" onChange = {(e) => {this.setState({"108": e.target.checked})}} />
-	    				<Form.Check id= {8} defaultChecked = "true" type="checkbox" inline label="Health & Wellness" onChange = {(e) => {this.setState({"107": e.target.checked})}}/>
-	    				<Form.Check id= {9} defaultChecked = "true" type="checkbox" inline label="Science & Technology" onChange = {(e) => {this.setState({"102": e.target.checked})}}/> 
-	    				<Form.Check id= {10} defaultChecked = "true" type="checkbox" inline label="Travel & Outdoor" onChange = {(e) => {this.setState({"109": e.target.checked})}}/>
-	    				<Form.Check id= {11} defaultChecked = "true" type="checkbox" inline label="Charity & Causes" onChange = {(e) => {this.setState({"111": e.target.checked})}}/>
-	    				<Form.Check id= {12} defaultChecked = "true" type="checkbox" inline label="Religion & Spirituality" onChange = {(e) => {this.setState({"114": e.target.checked})}}/> 
-	    				<Form.Check id= {13} defaultChecked = "true" type="checkbox" inline label="Family & Education" onChange = {(e) => {this.setState({"115": e.target.checked})}}/>
-	    				<Form.Check id= {14} defaultChecked = "true" type="checkbox" inline label="Seasonal & Holiday" onChange = {(e) => {this.setState({"116": e.target.checked})}}/>
-	    				<Form.Check id= {15} defaultChecked = "true" type="checkbox" inline label="Government & Politics" onChange = {(e) => {this.setState({"112": e.target.checked})}}/> 
-	    				<Form.Check id= {16} defaultChecked = "true" type="checkbox" inline label="Fashion & Beauty" onChange = {(e) => {this.setState({"106": e.target.checked})}}/>
-	    				<Form.Check id= {17} defaultChecked = "true" type="checkbox" inline label="Home & Lifestyle" onChange = {(e) => {this.setState({"117": e.target.checked})}}/>
-	    				<Form.Check id= {18} defaultChecked = "true" type="checkbox" inline label="Auto, Boat & Air" onChange = {(e) => {this.setState({"118": e.target.checked})}}/> 
-	    				<Form.Check id= {19} defaultChecked = "true" type="checkbox" inline label="Hobbies & Special Interest" onChange = {(e) => {this.setState({"119": e.target.checked})}}/>
-	    				<Form.Check id= {20} defaultChecked = "true" type="checkbox" inline label="Other" onChange = {(e) => {this.setState({"199": e.target.checked})}}/>
-	    				<Form.Check id= {21} defaultChecked = "true" type="checkbox" inline label="School Activities "onChange = {(e) => {this.setState({"120": e.target.checked})}} /> 
-
-	  				</Form.Group>
-				</Popover>}>
-    				<Button variant="link" className ="btn-cat">Choose categories</Button>
-  					</OverlayTrigger>
-  				</Col>
-  				</Row>
+    componentDidMount() {
+        this.input.focus()
+    }
 
 
+    render() {
+        return (
 
-			</Form>
-/*			<div>
-				<div>
-					<h1> JÄVLAR VAD MYCKET FIELDS DETTA ÄR </h1>
-					
-				</div>
-				<div class = "form-group">
-					<Form>
-						<Form.Group>
-						<Form.Control type="text" placeholder = "Type your search phrase here"/>
-						<input type = "text" class = "form-control" placeholder = "Type your search phrase here"/>
-						</Form.Group>
-					</Form>
-				</div>
-				<div>
-					<p>
-					<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-    					Button with data-target
-  					</button>
-  					</p>
-  					<div class="collapse" id="collapseExample">
- 						<div class="card card-body">
-    						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-  						</div>
-					</div>
-				</div>
+            <Form className="searchField" onSubmit={(e) => {
+                this.onClickPreventDefault(e);
+            }}>
+                <Form.Group>
+                    <Form.Control ref={(e) => {
+                        this.input = e
+                    }} type="text" placeholder="What event are you looking for?" onChange={(e) => {
+                        this.setState({query: e.target.value})
+                    }}/>
+                </Form.Group>
 
+                <Row className="justify-content-md-center">
+                    <Col>
+                        <OverlayTrigger trigger="click" placement="auto" rootClose="true" overlay={
+                            <Popover>
+                                <Calendar onClickDay={this.updateCalendar} value={this.state.fromDate}/>
+                            </Popover>}>
+                            <Button variant="link" className="btn-cat">From date
+                                : {this.state.fromDate.toLocaleDateString()}</Button>
+                        </OverlayTrigger>
+                    </Col>
+                    <Col>
+                        <OverlayTrigger trigger="click" placement="auto" rootClose="true" overlay={<Popover>
+                            <Calendar onClickDay={this.updateCalendar2} value={this.state.toDate}/>
+                        </Popover>}>
+                            <Button variant="link" className="btn-cat">To date
+                                : {this.state.toDate.toLocaleDateString()}</Button>
+                        </OverlayTrigger>
+                    </Col>
+                    <Col>
+                        <OverlayTrigger trigger="click" placement="auto" rootClose="true" overlay={<Popover>
+                            <Form.Check id={22} type="checkbox" inline label="Search Nearby Events"
+                                        value={this.state.isLocataionChecked}
+                                   onChange={(e) => {
+                                       this.setState({isLocataionChecked: e.target.value})
+                                   }}
+                            />
+                        </Popover>}>
+                            <Button variant="link" className="btn-cat">Search Nearby Events</Button>
+                        </OverlayTrigger>
+                    </Col>
+                    <Col>
+                        <OverlayTrigger trigger="click" placement="under" rootClose="true" overlay={<Popover>
+                            <Form.Group controlId="formBasicChecbox">
+                                <Form.Check id={1} type="checkbox" inline label="Music"
+                                            onChange={(e) => {
+                                                this.setState({music: "103"})
+                                            }}/>
+                                <Form.Check id={2}  type="checkbox" inline
+                                            label="Business & Professional" onChange={(e) => {
+                                    this.setState({businessAndProfessional: "101"})
+                                }}/>
+                                <Form.Check id={3}  type="checkbox" inline label="Food & Drink"
+                                            onChange={(e) => {
+                                                this.setState({foodAndDrink: "110"})
+                                            }}/>
+                                <Form.Check id={4}  type="checkbox" inline
+                                            label="Community & Culture" onChange={(e) => {
+                                    this.setState({communityAndCulture: "113"})
+                                }}/>
+                                <Form.Check id={5}  type="checkbox" inline
+                                            label="Performing & Visual Arts" onChange={(e) => {
+                                    this.setState({performingAndVisualArts: "105"})
+                                }}/>
+                                <Form.Check id={6}  type="checkbox" inline
+                                            label="Film, Media & Entertainment" onChange={(e) => {
+                                    this.setState({filmAndMediaAndEntertainment: "104"})
+                                }}/>
+                                <Form.Check id={7}  type="checkbox" inline
+                                            label="Sports & Fitness"
+                                            onChange={(e) => {
+                                                this.setState({sportsAndFitness: "108"})
+                                            }}/>
+                                <Form.Check id={8}  type="checkbox" inline
+                                            label="Health & Wellness" onChange={(e) => {
+                                    this.setState({healthAndWellness: "107"})
+                                }}/>
+                                <Form.Check id={9}  type="checkbox" inline
+                                            label="Science & Technology" onChange={(e) => {
+                                    this.setState({scienceAndTechnology: "102"})
+                                }}/>
+                                <Form.Check id={10}  type="checkbox" inline
+                                            label="Travel & Outdoor" onChange={(e) => {
+                                    this.setState({travelAndOutdoor: "109"})
+                                }}/>
+                                <Form.Check id={11}  type="checkbox" inline
+                                            label="Charity & Causes" onChange={(e) => {
+                                    this.setState({charityAndCauses: "111"})
+                                }}/>
+                                <Form.Check id={12}  type="checkbox" inline
+                                            label="Religion & Spirituality" onChange={(e) => {
+                                    this.setState({religionAndSpirituality: "114"})
+                                }}/>
+                                <Form.Check id={13}  type="checkbox" inline
+                                            label="Family & Education" onChange={(e) => {
+                                    this.setState({familyAndEducation: "115"})
+                                }}/>
+                                <Form.Check id={14}  type="checkbox" inline
+                                            label="Seasonal & Holiday" onChange={(e) => {
+                                    this.setState({seasonalAndHoliday: "116"})
+                                }}/>
+                                <Form.Check id={15}  type="checkbox" inline
+                                            label="Government & Politics" onChange={(e) => {
+                                    this.setState({governmentAndPolitics: "112"})
+                                }}/>
+                                <Form.Check id={16}  type="checkbox" inline
+                                            label="Fashion & Beauty" onChange={(e) => {
+                                    this.setState({fashionAndBeauty: "106"})
+                                }}/>
+                                <Form.Check id={17}  type="checkbox" inline
+                                            label="Home & Lifestyle" onChange={(e) => {
+                                    this.setState({homeAndLifestyle: "117"})
+                                }}/>
+                                <Form.Check id={18}  type="checkbox" inline
+                                            label="Auto, Boat & Air" onChange={(e) => {
+                                    this.setState({autoAndBoatAndAir: "118"})
+                                }}/>
+                                <Form.Check id={19}  type="checkbox" inline
+                                            label="Hobbies & Special Interest" onChange={(e) => {
+                                    this.setState({hobbiesAndSpecialAndInterest: "119"})
+                                }}/>
+                                <Form.Check id={20}  type="checkbox" inline label="Other"
+                                            onChange={(e) => {
+                                                this.setState({other: "199"})
+                                            }}/>
+                                <Form.Check id={21}  type="checkbox" inline
+                                            label="School Activities" onChange={(e) => {
+                                    this.setState({schoolActivities: "120"})
+                                }}/>
 
-			</div>
-*/
-			)
-	}
+                            </Form.Group>
+                        </Popover>}>
+                            <Button variant="link" className="btn-cat">Choose categories</Button>
+                        </OverlayTrigger>
+                    </Col>
+                </Row>
+            </Form>
+        )
+    }
 }
 
 export default geolocated({
-  positionOptions: {
-    enableHighAccuracy: false,
-  },
-  userDecisionTimeout: 5000,
+    positionOptions: {
+        enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000,
 })(SearchField);
 
